a434a9c3f7c84fa90e0c42ee3fc2e8369a9857d7
Dongook Son <60206749+donny-son@users.noreply.github.com>
2022-09-21 22:12:32 +0900

    add rich table formatting

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 build:
-	pex \
+	poetry run pex \
 	--requirement=requirements.txt \
 	--sources-directory=. \
 	--output-file=dist/chkmail \
 	-m chkmail
+
+install:
+	sudo cp ./dist/chkmail /usr/local/bin
--- chkmail/__main__.py
+++ chkmail/__main__.py
@@ -1,10 +1,18 @@
 from validate_email import validate_email
 import typer
+from rich.table import Table
+from rich.console import Console
+
+console = Console()
+table = Table(title="E-Mail Checker", style="magenta")
 
 
 def main(email: str):
     is_valid = validate_email(email_address=email)
-    typer.echo(f"{email} is {'valid ✅' if is_valid else 'invalid 🚫'}")
+    table.add_column("E-Mail", justify="center", style="cyan", no_wrap=True)
+    table.add_column("Validity", justify="center")
+    table.add_row(email, "✅" if is_valid else "🚫")
+    console.print(table)
 
 
 typer.run(main)
